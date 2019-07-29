af229edb730b88b39eb791e0ccd1ab284cf235b1
Dmitriy Khoroshev <dkhoroshev@sfbtech.org>
2019-07-29 17:43:51 +0300

    Домашнее задание №4.

---
+++ src/lesson4/homework/JavaFxApp.java
@@ -0,0 +1,19 @@
+package lesson4.homework;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class JavaFxApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Network Chat");
+        primaryStage.setScene(new Scene(new StackPane(), 300, 300));
+        primaryStage.show();
+    }
+}
