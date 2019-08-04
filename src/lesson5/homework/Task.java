e776825f04d7fcc2c4e07fb2ceec2975bc6ccc27
Dmitriy Khoroshev <dkhoroshev@sfbtech.org>
2019-08-04 13:42:28 +0300

    Добавлен урок №6

---
+++ src/lesson5/homework/Task.java
@@ -0,0 +1,59 @@
+package lesson5.homework;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+// Task class to be executed (Step 1)
+class Task implements Runnable
+{
+    private String name;
+    private float elem;
+
+    public Task(String s, float a)
+    {
+        name = s;
+        elem = a;
+    }
+//    private static void calc(float elem) {
+//
+//        for (int i = 0; i < elem.length; i++) {
+//            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+//        }
+//    }
+
+    // Prints task name and sleeps for 1s
+    // This Whole process is repeated 5 times
+    public void run()
+    {
+        try
+        {
+            for (int i = 0; i<=5; i++)
+            {
+                if (i==0)
+                {
+                    Date d = new Date();
+                    SimpleDateFormat ft = new SimpleDateFormat("hh:mm:ss");
+                    System.out.println("Initialization Time for"
+                            + " task name - "+ name +" = " +ft.format(d));
+                    //prints the initialization time for every task
+                }
+                else
+                {
+                    Date d = new Date();
+                    SimpleDateFormat ft = new SimpleDateFormat("hh:mm:ss");
+                    System.out.println("Executing Time for task name - "+
+                            name +" = " +ft.format(d));
+                    // prints the execution time for every task
+                }
+                Thread.sleep(1000);
+            }
+            System.out.println(name+" complete");
+        }
+
+        catch(InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
